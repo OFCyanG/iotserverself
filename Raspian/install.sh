a76b5a5dc597a1e42a4eb20120f3bc1aee2dcfbd
OFCyanG <37908480+OFCyanG@users.noreply.github.com>
2019-04-08 18:26:57 +0700

    update

--- Raspian/install.sh
+++ Raspian/install.sh
@@ -40,6 +40,7 @@ sudo npm install -g --unsafe-perm node-red node-red-admin --allow-root
 script_node_red_services="[Unit]\nDescription=Node-RED\nAfter=syslog.target network.target\n\n[Service]\nExecStart=`which 'node-red'`-pi --max-old-space-size=128 -v\nRestart=on-failure\nKillSignal=SIGINT\nSyslogIdentifier=node-red\nStandardOutput=syslog\nWorkingDirectory=/home/`echo $USER`/\nUser=`echo $USER`\n\n[Install]\nWantedBy=multi-user.target"
 
 echo -e $script_node_red_services > ./node-red.service
+sudo cp -f ./node-red.service /etc/systemd/system/node-red.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable node-red.service
